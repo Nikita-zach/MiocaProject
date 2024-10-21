33317b66aaa915a3c0f477b482ed8fda4cd30dc0
Nikita-zach <akhmetshynm@gmail.com>
2024-10-22 01:59:51 +0200

    complete editing products.html and completing models for it

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -24,6 +25,8 @@ class Products(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     photo = models.ImageField(upload_to='products', blank=True, null=True)
 
     is_visible = models.BooleanField(default=True)
@@ -34,8 +37,32 @@ class Products(models.Model):
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
 
+    def final_price(self):
+        if self.discount_price is not None:
+            return self.discount_price
+        elif self.discount_percentage is not None:
+            return self.price * (1 - (self.discount_percentage / 100))
+        return self.price
+
+    def average_rating(self):
+        reviews = self.reviews.all()
+        if reviews:
+            return sum(review.rating for review in reviews) / reviews.count()
+        return 0
+
     def __str__(self):
         return self.name
+
+
+    class Review(models.Model):
+        product = models.ForeignKey('Products', on_delete=models.CASCADE, related_name='reviews')
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        rating = models.IntegerField()
+        comment = models.TextField(blank=True, null=True)
+        created_at = models.DateTimeField(auto_now_add=True)
+
+        def __str__(self):
+            return f'Review by {self.user.username} for {self.product.name}'
 
 
 class Cart(models.Model):
--- home/views.py
+++ home/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from .models import Category, Products, Cart, Wishlist
+
 
 def home(request):
-    return render(request, 'main.html')
+
+    categories = Category.objects.filter(is_visible=True).order_by('sort')
+    products = Products.objects.filter(is_visible=True).order_by('sort')
+
+    if request.user.is_authenticated:
+        cart = Cart.objects.get(user=request.user)
+        wishlist = Wishlist.objects.get(user=request.user)
+    else:
+        cart = None
+        wishlist = None
+
+    context = {
+        "categories": categories,
+        "products": products,
+        "cart": cart,
+        "wishlist": wishlist,
+        "book_table_form": book_table_form
+    }
+
+    return render(request, "index.html", context)
+
+
+def thanks(request):
+    return render(request, 'thanks.html')
