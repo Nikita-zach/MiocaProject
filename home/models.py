1b954645e8e7f44240b35168aa11cb1fd970e364
Nikita-zach <akhmetshynm@gmail.com>
2024-10-28 18:21:27 +0100

    add contact form and edit html for it

--- home/models.py
+++ home/models.py
@@ -13,12 +13,3 @@ class Feature(models.Model):
 
     def __str__(self):
         return self.name
-
-class FAQ(models.Model):
-    question = models.CharField(max_length=255)
-    answer = models.CharField(max_length=255)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    def __str__(self):
-        return self.question
