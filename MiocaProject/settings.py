e4106a18ae895409e42061e1c092856da0a7e061
Nikita-zach <akhmetshynm@gmail.com>
2024-11-11 22:57:00 +0100

    Fix problem with secret key

--- MiocaProject/settings.py
+++ MiocaProject/settings.py
@@ -14,13 +14,15 @@ from pathlib import Path
 import os
 from django.conf import settings
 from django.conf.urls.static import static
+import dotenv
+from dotenv import load_dotenv
 
-
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 DEBUG = True
 
